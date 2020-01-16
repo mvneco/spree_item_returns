bdeb9c0b469ac873756723b9afd8bf689126d5c3
igor-djuric-sedecom <59830974+igor-djuric-sedecom@users.noreply.github.com>
2020-01-16 13:52:04 +0100

    Made compatible with Zeitwerk class loader

--- app/models/spree/return_authorization_decorator.rb
+++ app/models/spree/return_authorization_decorator.rb
@@ -1,24 +1,28 @@
-Spree::ReturnAuthorization.class_eval do
+module Spree
+  module ReturnAuthorizationDecorator
+  
+    def prepended(base)
+      base.stock_location_validations = base._validators[:stock_location]
+      if base.stock_location_validations.present?
+        base.stock_location_validations.reject! { |validation| validation.is_a? ActiveRecord::Validations::PresenceValidator }
+      end
 
-  stock_location_validations = _validators[:stock_location]
-  if stock_location_validations.present?
-    stock_location_validations.reject! { |validation| validation.is_a? ActiveRecord::Validations::PresenceValidator }
-  end
+      base._validate_callbacks.each do |callback|
+        callback.raw_filter.attributes.delete :stock_location if callback.raw_filter.is_a?(ActiveModel::Validations::PresenceValidator)
+      end
 
-  _validate_callbacks.each do |callback|
-    callback.raw_filter.attributes.delete :stock_location if callback.raw_filter.is_a?(ActiveModel::Validations::PresenceValidator)
-  end
+      base.validates :stock_location, presence: true, unless: :user_initiated?
+      base.validates :return_items, presence: true, if: :user_initiated?
 
-  validates :stock_location, presence: true, unless: :user_initiated?
-  validates :return_items, presence: true, if: :user_initiated?
+      base.after_commit :notify_admin, :notify_user, on: :create, if: :user_initiated?
+    end
 
-  after_commit :notify_admin, :notify_user, on: :create, if: :user_initiated?
+    def allow_return_item_changes?
+      !customer_returned_items?
+    end
 
-  def allow_return_item_changes?
-    !customer_returned_items?
-  end
+    private
 
-  private
     def notify_admin
       Spree::ReturnAuthorizationMailer.notify_return_initialization_to_admin(number).deliver_later
     end
@@ -27,4 +31,7 @@ Spree::ReturnAuthorization.class_eval do
       Spree::ReturnAuthorizationMailer.notify_return_initialization_to_user(number).deliver_later
     end
 
+  end
 end
+
+Spree::ReturnAuthorization.prepend(Spree::ReturnAuthorizationDecorator)
