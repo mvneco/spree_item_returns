fb4a7e4a56c1f59999cdb691b54f0af6fd03ea73
igor-djuric-sedecom <59830974+igor-djuric-sedecom@users.noreply.github.com>
2020-01-20 09:27:02 +0100

    Prevented multiple invocations of self.prepended(base)

--- app/models/spree/return_authorization_decorator.rb
+++ app/models/spree/return_authorization_decorator.rb
@@ -1,7 +1,15 @@
 module Spree
   module ReturnAuthorizationDecorator
   
-    def prepended(base)
+    # Crucial to prevent multiple invocations of self.prepended(base)!
+    @@already_prepended = false unless defined? @@already_prepended
+
+    def self.prepended(base)
+
+      # Crucial to prevent multiple invocations of self.prepended(base)!
+      return if self.class_variable_get(:@@already_prepended)
+      self.class_variable_set(:@@already_prepended, true)
+
       base.stock_location_validations = base._validators[:stock_location]
       if base.stock_location_validations.present?
         base.stock_location_validations.reject! { |validation| validation.is_a? ActiveRecord::Validations::PresenceValidator }
