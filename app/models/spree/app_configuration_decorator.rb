66ff486cad7989da8aed27f81359ba013d4e0209
igor-djuric-sedecom <59830974+igor-djuric-sedecom@users.noreply.github.com>
2020-01-20 08:54:45 +0100

    Made compatible with Zeitwerk class loader

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -1,3 +1,10 @@
-Spree::AppConfiguration.class_eval do
-  preference :return_initiation_admin_mail_address, :string, default: 'spree@example.com'
+module Spree
+  module AppConfigurationDecorator
+
+    def prepended(base)
+      base.preference :return_initiation_admin_mail_address, :string, default: 'spree@example.com'
+    end
+  end
 end
+
+Spree::AppConfiguration.prepend(Spree::AppConfigurationDecorator)
