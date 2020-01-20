10925b9ccd48ed17f3631abf7f5c78b0aee1c5ee
igor-djuric-sedecom <59830974+igor-djuric-sedecom@users.noreply.github.com>
2020-01-20 09:26:29 +0100

    Prevented multiple invocations of self.prepended(base)

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,7 +1,15 @@
 module Spree
   module ProductDecorator
   
-    def prepended(base)
+    # Crucial to prevent multiple invocations of self.prepended(base)!
+    @@already_prepended = false unless defined? @@already_prepended
+
+    def self.prepended(base)
+
+      # Crucial to prevent multiple invocations of self.prepended(base)!
+      return if self.class_variable_get(:@@already_prepended)
+      self.class_variable_set(:@@already_prepended, true)
+
       base.validates :return_time, numericality: { greater_than_or_equal_to: 0 }
       base.scope :returnable, -> { where(returnable: true) }
     end
