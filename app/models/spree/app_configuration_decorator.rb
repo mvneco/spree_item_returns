e7a77bf56f110fdb567fe7ed4cd4064dc24419e3
igor-djuric-sedecom <59830974+igor-djuric-sedecom@users.noreply.github.com>
2020-01-20 09:25:06 +0100

    Prevented multiple invocations of self.prepended(base)

--- app/models/spree/app_configuration_decorator.rb
+++ app/models/spree/app_configuration_decorator.rb
@@ -1,7 +1,15 @@
 module Spree
   module AppConfigurationDecorator
 
-    def prepended(base)
+    # Crucial to prevent multiple invocations of self.prepended(base)!
+    @@already_prepended = false unless defined? @@already_prepended
+
+    def self.prepended(base)
+
+      # Crucial to prevent multiple invocations of self.prepended(base)!
+      return if self.class_variable_get(:@@already_prepended)
+      self.class_variable_set(:@@already_prepended, true)
+ 
       base.preference :return_initiation_admin_mail_address, :string, default: 'spree@example.com'
     end
   end
