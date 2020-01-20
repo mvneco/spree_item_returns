ea9046e90e3eaed015d52618c34b006d05153625
igor-djuric-sedecom <59830974+igor-djuric-sedecom@users.noreply.github.com>
2020-01-20 13:18:47 +0100

    Added diagnostics

--- app/models/spree/app_configuration_spree_item_returns_decorator.rb
+++ app/models/spree/app_configuration_spree_item_returns_decorator.rb
@@ -15,8 +15,12 @@ module Spree
       puts "Spree::AppConfigurationSpreeItemReturnsDecorator.self.prepended(base) :: A3 #{self.inspect}"
       base.preference :return_initiation_admin_mail_address, :string, default: 'spree@example.com'
       puts 'Spree::AppConfigurationSpreeItemReturnsDecorator.self.prepended(base) :: B1'
-      puts "Spree::AppConfigurationSpreeItemReturnsDecorator.self.prepended(base) :: B2 #{base.has_preference?('return_initiation_admin_mail_address')}"
-      puts "Spree::AppConfigurationSpreeItemReturnsDecorator.self.prepended(base) :: B3 #{base.has_preference?(:return_initiation_admin_mail_address)}"
+      puts "Spree::AppConfigurationSpreeItemReturnsDecorator.self.prepended(base) :: B2 #{base}"
+      puts "Spree::AppConfigurationSpreeItemReturnsDecorator.self.prepended(base) :: B3 #{self}"
+      puts "Spree::AppConfigurationSpreeItemReturnsDecorator.self.prepended(base) :: B4 #{self.has_preference?('return_initiation_admin_mail_address')}"
+      puts "Spree::AppConfigurationSpreeItemReturnsDecorator.self.prepended(base) :: B5 #{self.has_preference?(:return_initiation_admin_mail_address)}"
+      puts "Spree::AppConfigurationSpreeItemReturnsDecorator.self.prepended(base) :: B6 #{base.has_preference?('return_initiation_admin_mail_address')}"
+      puts "Spree::AppConfigurationSpreeItemReturnsDecorator.self.prepended(base) :: B7 #{base.has_preference?(:return_initiation_admin_mail_address)}"
       
     end
   end
