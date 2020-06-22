14695906fd2a7dae9f2c92bcd51a61fccd69df40
igor-djuric <61732151+igor-djuric@users.noreply.github.com>
2020-06-22 09:45:09 +0200

    EB-13641: Made compatible with Ortel B2C Webshop

--- app/overrides/spree/return_authorization/user_return_authorization_history_button.rb
+++ app/overrides/spree/return_authorization/user_return_authorization_history_button.rb
@@ -2,8 +2,12 @@ Deface::Override.new(
   virtual_path: 'spree/users/show',
   name: 'user_return_authorization_history_button',
   insert_bottom: "[data-hook='account_my_orders'] h3",
-  text: "<%= link_to(spree.return_authorizations_path, class: 'btn btn-primary pull-right') do %>
-          <span class='glyphicon glyphicon-list-alt'></span>
-          <%= Spree.t(:returns_history) %>
-        <% end %>"
+  text: "<button id='returns_history_btn' type='button' class='btn btn-outline-primary mx-4'>
+          <i class='zmdi zmdi-undo'></i> <%= Spree.t(:returns_history) %>
+         </button>
+        <script>
+          $('button#returns_history_btn').click(function(e) {
+            window.location = '<%= spree.return_authorizations_path %>';
+          });
+        </script>"
 )
